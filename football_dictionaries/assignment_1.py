acec0ed840300916f90918728847432489e77733
AbdulrhmanAwadh <abdulrhmanawadh1@gmail.com>
2024-11-27 03:41:59 +0300

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,26 @@
 def players_as_dictionaries(squads_list):
-    pass
+    SQUADS_DATA = [
+  [
+    "1", "GK", "Juan Botasso", "(1908-10-23)23 October 1908 (aged 21)", "", "Quilmes", "Argentina", "Argentina", "1930"
+  ],
+  [
+    "9", "FW", "Roberto Cherro", "(1907-02-23)23 February 1907 (aged 23)", "", "Boca Juniors", "Argentina", "Argentina", "1930"
+  ]
+]
+
+players_dicts = [
+    {
+        'number': player[0],
+        'position': player[1],
+        'name': player[2],
+        'date_of_birth': player[3],
+        'caps': player[4],
+        'club': player[5],
+        'country': player[6],
+        'club_country': player[7],
+        'year': player[8]
+    } for player in SQUADS_DATA
+]
+
+print(players_dicts)
+
